dd68e5ec328662a75f885eb3410c716295f29fc3
Greg Kappatos <sharpdressedcodes@users.noreply.github.com>
2022-09-12 23:57:07 +1000

    Setup git hooks

--- scripts/install-husky.js
+++ scripts/install-husky.js
@@ -4,6 +4,7 @@ const isCi = process.env.CI !== undefined;
 const isDocker = process.env.IS_DOCKER !== undefined;
 
 if (!isCi && !isDocker) {
+    // eslint-disable-next-line global-require
     require('husky').install();
 }
 
