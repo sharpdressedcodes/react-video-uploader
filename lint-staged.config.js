e86456de923654da45a03b74d7c524cf1ba59bf2
Greg Kappatos <sharpdressedcodes@users.noreply.github.com>
2022-09-13 12:27:59 +1000

    Add markdownlint config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,31 +1,31 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const micromatch = require('micromatch');
 
+// Stylelint doesn't end properly when there are no files given.
+// Only run linters when there are actual files that have changed.
 module.exports = allStagedFiles => {
     const codeFiles = micromatch(allStagedFiles, ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.ts']);
     const markdownFiles = micromatch(allStagedFiles, ['**/*.md']);
     const styleFiles = micromatch(allStagedFiles, ['**/*.scss', '**/*.css']);
-    const linters = [];
+    const linters = [
+        markdownFiles?.length ? `npm run lint:md:staged -- ${markdownFiles.join(' ')}` : [],
+        codeFiles?.length ? `npm run lint:js:staged -- ${codeFiles.join(' ')}` : [],
+        styleFiles?.length ? `npm run lint:scss:staged -- ${styleFiles.join(' ')}` : []
+    ].flat();
+
+    console.log('linters', linters);
 
     // if (markdownFiles?.length) {
-    //     console.log('Found markdownFiles for linting', markdownFiles);
     //     linters.push(`npm run lint:md:staged -- ${markdownFiles.join(' ')}`);
     // }
     //
     // if (codeFiles?.length) {
-    //     console.log('Found codeFiles for linting', codeFiles);
     //     linters.push(`npm run lint:js:staged -- ${codeFiles.join(' ')}`);
     // }
     //
     // if (styleFiles?.length) {
-    //     console.log('Found styleFiles for linting', styleFiles);
     //     linters.push(`npm run lint:scss:staged -- ${styleFiles.join(' ')}`);
     // }
 
     return linters;
-    // return [
-    //     `npm run lint:md:staged -- ${markdownFiles.join(' ')}`,
-    //     `npm run lint:js:staged -- ${codeFiles.join(' ')}`,
-    //     `npm run lint:scss:staged -- ${styleFiles.join(' ')}`
-    // ];
 };
